7329bdffdce772b519a89553e2fc3b188e876eef
Eloy Duran <eloy.de.enige@gmail.com>
2009-01-16 00:30:46 +0100

    Added Gem::Installer#download_to which uses simple 'ol curl to download the gem.

--- lib/microgem/installer.rb
+++ lib/microgem/installer.rb
@@ -9,6 +9,11 @@ module Gem
       def url
         "http://gems.rubyforge.org/gems/#{@gem_spec.gem_filename}"
       end
+      
+      # Downloads the gem into the +download_dir+.
+      def download_to(download_dir)
+        system "/usr/bin/curl -o '#{ File.join(download_dir, @gem_spec.gem_filename) }' #{url}"
+      end
     end
   end
 end
--- test/installer_test.rb
+++ test/installer_test.rb
@@ -3,8 +3,22 @@
 require File.expand_path('../test_helper', __FILE__)
 
 describe "Gem::Micro::Installer" do
+  def setup
+    @gem_spec = Gem::SourceIndex.instance.gem_specs('rake').last
+    @installer = Gem::Micro::Installer.new(@gem_spec)
+  end
+  
   it "should return the url to download the gem from" do
-    gem_spec = Gem::SourceIndex.instance.gem_specs('rake').last
-    Gem::Micro::Installer.new(gem_spec).url.should == "http://gems.rubyforge.org/gems/rake-0.8.1.gem"
+    @installer.url.should == "http://gems.rubyforge.org/gems/rake-0.8.1.gem"
+  end
+  
+  it "should download the gem with curl" do
+    def @installer.system(command)
+      @command = command
+    end
+    
+    @installer.download_to('/path/to/download/dir')
+    @installer.instance_variable_get(:@command).should ==
+      "/usr/bin/curl -o '/path/to/download/dir/rake-0.8.1.gem' #{@installer.url}"
   end
 end
