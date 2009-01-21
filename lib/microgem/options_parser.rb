f04a02520f6f0bb4e20de7bce3211221100b0948
Eloy Duran <eloy.de.enige@gmail.com>
2009-01-21 22:39:04 +0100

    Oops, forgot to add options_parser.rb. Options changed to OptionsParser.

---
+++ lib/microgem/options_parser.rb
@@ -0,0 +1,59 @@
+require 'optparse'
+
+module Gem
+  module Micro
+    class OptionsParser
+      attr_accessor :command, :arguments, :options
+      
+      def initialize
+        @options = {}
+      end
+      
+      def parser
+        @parser ||= OptionParser.new do |opts|
+          opts.banner =  "Microgem is an unsophisticated package manager for Ruby."
+          opts.separator "And the first commandline utility to start with a multibyte character; µ"
+          opts.separator ""
+          opts.separator "  Usage:"
+          opts.separator "        µgem [command] [arguments…] [options…]"
+          opts.separator ""
+          opts.separator "  Example:"
+          opts.separator "        µgem install rake"
+          opts.separator "        µgem install rails --force"
+          opts.separator "        µgem cache update --debug"
+          opts.separator ""
+          opts.separator "  Options:"
+          
+          opts.on("--debug", "Raises the log level to `debug'") do
+            @options[:log_level] = :debug
+          end
+          
+          opts.on("--force", "Forces a command") do
+            @options[:force] = true
+          end
+          
+          opts.on("--simple-downloader", "Use curl to download files instead of Net::HTTP") do
+            @options[:simple_downloader] = true
+          end
+          
+          opts.on("--help", "Show help information") do
+            puts opts
+            exit
+          end
+          
+        end
+      end
+      
+      def banner
+        parser.to_s
+      end
+      
+      def parse(arguments)
+        parser.parse! arguments
+        self.command = arguments.shift
+        self.arguments = arguments
+        self
+      end
+    end
+  end
+end
---
+++ test/options_parser_test.rb
@@ -0,0 +1,27 @@
+##!/usr/bin/env macruby
+
+require File.expand_path('../test_helper', __FILE__)
+
+describe "Gem::Micro::Options, with options" do
+  def setup
+    @parser = Gem::Micro::OptionsParser.new
+  end
+  
+  it "should set the log_level" do
+    @parser.parse(%w{ install rake --debug }).options.should == { :log_level => :debug }
+  end
+  
+  it "should force commands" do
+    @parser.parse(%w{ install rake --force }).options.should == { :force => true }
+  end
+  
+  it "should enable the simple downloader" do
+    @parser.parse(%w{ install rake --simple-downloader }).options.should == { :simple_downloader => true }
+  end
+  
+  it "should return the command and its arguments" do
+    @parser.parse(%w{ install rake --debug })
+    @parser.command.should == 'install'
+    @parser.arguments.should == ['rake']
+  end
+end
